9e477ff9a64772b19942715aa0828b322d562cf7
leonardo lima <leo.lima21051990@gmail.com>
2021-01-05 13:56:37 -0300

    add real points

--- scripts/challenge.py
+++ scripts/challenge.py
@@ -23,13 +23,13 @@ class Challenge:
   def callback(self, data):    
     if (data.data == 0):
       print('[info] ID 0 detected')
-      self.goal_move_base(0.5,0)
+      self.goal_move_base(3.14, -3.48, 0.77, 0.63)
     elif (data.data == 1):
       print('[info] ID 1 detected')
-      self.goal_move_base(-0.5,0)
+      self.goal_move_base(5.79, -2.12, 0.17, 0.98)
     elif (data.data == 2):
       print('[info] ID 2 detected')
-      self.goal_move_base(0,0)
+      self.goal_move_base(1.07, -2.17, -0.99, 0.06)
     else:
       print('[info] Unknow error ou unknow tag detected')
 
@@ -40,12 +40,13 @@ class Challenge:
     # Mantem o python funcionando apos o encerramendo do node
     rospy.spin()
   
-  def goal_move_base(self, pose_x, pose_y):
+  def goal_move_base(self, pose_x, pose_y, pose_z, pose_w):
     # setup pub values with x and y positions
     msg_move_to_goal = PoseStamped()
     msg_move_to_goal.pose.position.x = pose_x 
     msg_move_to_goal.pose.position.y = pose_y
-    msg_move_to_goal.pose.orientation.w = 1
+    msg_move_to_goal.pose.orientation.z = pose_z
+    msg_move_to_goal.pose.orientation.w = pose_w
     msg_move_to_goal.header.frame_id = 'map'
     # msg_move_to_goal.header.frame_id = 'base_footprint'
 
