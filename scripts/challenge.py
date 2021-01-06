88a22aac95384afcc14f0296ee4f62288f7dfe6e
leonardo lima <leo.lima21051990@gmail.com>
2021-01-06 16:07:28 -0300

    add points for new map and a random move

--- scripts/challenge.py
+++ scripts/challenge.py
@@ -5,6 +5,8 @@ import math
 import time
 import rospy
 import numpy as np
+import random
+from time import sleep
 from std_msgs.msg import Int32
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
@@ -20,16 +22,30 @@ class Challenge:
     self.move_base_pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
     rospy.init_node('challenge_movebase', anonymous=True)
 
+    self.rand_start_pose = random.randint(0,2)
+
+    if (self.rand_start_pose == 0):
+      print('[info] moving to point 0')
+      self.goal_move_base(1.85, 0.14, 0.71, 0.69)
+    elif (self.rand_start_pose == 1):
+      print('[info] moving to point 1')
+      self.goal_move_base(-1.37, -0.43, -0.72, 0.68)
+    elif (self.rand_start_pose == 2):
+      print('[info] moving to point 2')
+      self.goal_move_base(-0.36, 1.34, 0.73, 0.67)
+    else:
+      print('[info] Unknow error detected')
+
   def callback(self, data):    
     if (data.data == 0):
       print('[info] ID 0 detected')
-      self.goal_move_base(3.14, -3.48, 0.77, 0.63)
+      self.goal_move_base(-1.37, -0.43, -0.72, 0.68)
     elif (data.data == 1):
       print('[info] ID 1 detected')
-      self.goal_move_base(5.79, -2.12, 0.17, 0.98)
+      self.goal_move_base(-0.36, 1.34, 0.73, 0.67)
     elif (data.data == 2):
       print('[info] ID 2 detected')
-      self.goal_move_base(1.07, -2.17, -0.99, 0.06)
+      self.goal_move_base(1.85, 0.14, 0.71, 0.69)
     else:
       print('[info] Unknow error ou unknow tag detected')
 
@@ -51,6 +67,8 @@ class Challenge:
     # msg_move_to_goal.header.frame_id = 'base_footprint'
 
     self.move_base_pub.publish(msg_move_to_goal)
+    sleep(20)
+    # add aqui a espera pelo resutado do move base
 
 # Funcao main
 if __name__ == '__main__':
