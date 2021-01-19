b5a3c22f21e126f0c8a907f017fa9f2c8d2e7070
leonardo lima <leo.lima21051990@gmail.com>
2021-01-19 11:13:37 -0300

    update and fix errors in challenge_final code

--- scripts/challenge_final.py
+++ scripts/challenge_final.py
@@ -24,19 +24,14 @@ import roslib
 
 class Challenge:
   def __init__(self):
-    print('[info] iniciando variaveis...')
+    print('[info] initializing vars...')
     
     rospy.init_node('challenge_movebase', anonymous=True) 
 
-    # testar o queue_size com 1 e 10
     self.move_base_pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
-    # self.velocity_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
     self.bridge = CvBridge()
     self.tflistener = tf.TransformListener()
-
-    # ------------------
-    # self.time_var = 300
 
     self.tag_0_status = False
     self.tag_1_status = False
@@ -45,6 +40,10 @@ class Challenge:
     self.tag_0_p = []
     self.tag_1_p = []
     self.tag_2_p = []
+
+    self.tag_0_go = False
+    self.tag_1_go = False
+    self.tag_2_go = False
 
     print('[info] variaveis iniciadas')
   
@@ -56,33 +55,56 @@ class Challenge:
       
       if tag_det == 0 and self.tag_0_status == False:
         self.tag_0_status = True
-        self.tag_0_p = [trans[0], trans[1], rot[2], rot[3]]
+        self.tag_0_p = [round(trans[0], 2), 
+                        round(trans[1], 2), 
+                        round(rot[2], 2),
+                        round(rot[3], 2)
+                        ]
         # self.goal_move_base(0, 0, -1, 1)
         print(self.tag_0_p)
 
       elif tag_det == 1 and self.tag_1_status == False:
         self.tag_1_status = True
-        self.tag_1_p = [trans[0], trans[1], rot[2], rot[3]]
+        self.tag_1_p = [round(trans[0], 2), 
+                        round(trans[1], 2), 
+                        round(rot[2], 2),
+                        round(rot[3], 2)
+                        ]
         # self.goal_move_base(0, 0, -1, 1)
         print(self.tag_1_p)
 
       elif tag_det == 2 and self.tag_2_status == False:
         self.tag_2_status = True
-        self.tag_2_p = [trans[0], trans[1], rot[2], rot[3]]
+        self.tag_2_p = [round(trans[0], 2), 
+                        round(trans[1], 2), 
+                        round(rot[2], 2),
+                        round(rot[3], 2)
+                        ]
         # self.goal_move_base(0, 0, -1, 1)
         print(self.tag_2_p)
 
     if self.tag_0_status == True and self.tag_1_status == True and self.tag_2_status == True:
       # rosnode kill explorer--------------------      
-      if (tag_det == 0):
-        print('[info] ID 0 detected')
+      if (tag_det == 0 and self.tag_1_go == False):
+        print('[info] ID 0 detected and going to tag 1')
         self.goal_move_base(self.tag_1_p[0], self.tag_1_p[1], self.tag_1_p[2], self.tag_1_p[3])
-      elif (tag_det == 1):
-        print('[info] ID 1 detected')
+        self.tag_0_go = False
+        self.tag_1_go = True
+        self.tag_2_go = False
+
+      elif (tag_det == 1 and self.tag_2_go == False):
+        print('[info] ID 1 detected and going to tag 2')
         self.goal_move_base(self.tag_2_p[0], self.tag_2_p[1], self.tag_2_p[2], self.tag_2_p[3])
-      elif (tag_det == 2):
-        print('[info] ID 2 detected')
+        self.tag_0_go = False
+        self.tag_1_go = False
+        self.tag_2_go = True
+
+      elif (tag_det == 2 and self.tag_0_go == False):
+        print('[info] ID 2 detected and going to tag 0')
         self.goal_move_base(self.tag_0_p[0], self.tag_0_p[1], self.tag_0_p[2], self.tag_0_p[3])
+        self.tag_0_go = True
+        self.tag_1_go = False
+        self.tag_2_go = False
 
   def listener(self):
     # Inscricao no topico e definicao da callback como funcao a ser executada
